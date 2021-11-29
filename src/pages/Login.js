949d14b64ecb1e333dedbf1e239a12f323932e92
raymndle <94584998+raymndle@users.noreply.github.com>
2021-11-29 15:41:16 +1100

    log and registration page
    Created with material UI. Next step is to include routing to make these links alternate dynamically.

---
+++ src/pages/Login.js
@@ -0,0 +1,79 @@
+import React from "react";
+import "./Login.css";
+import img1 from "../images/logo.png";
+import Button from "@mui/material/Button";
+import TextField from "@material-ui/core/TextField";
+import { AccountCircle, LockRounded } from "@material-ui/icons";
+import { InputAdornment } from "@mui/material";
+import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
+import { BrowserRouter as Router, Link as RouterLink } from "react-router-dom";
+
+function Login() {
+  return (
+    <div className="login-form">
+      <Box
+        height="75px"
+        alignItems="center"
+        display="flex"
+        flexDirection="column"
+        marginTop="100px"
+        padding="10px"
+      >
+        <img src={img1} alt="" />
+      </Box>
+      <Box
+        alignItems="center"
+        display="flex"
+        flexDirection="column"
+        marginTop="10px"
+        padding="20px"
+      >
+        <TextField
+          size="small"
+          variant="outlined"
+          color="primary"
+          type="email"
+          placeholder="Email"
+          label="Email"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <TextField
+          margin="normal"
+          size="small"
+          variant="outlined"
+          color="primary"
+          type="password"
+          placeholder="Password"
+          label="Password"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <LockRounded />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <Button variant="contained" size="medium">
+          Login
+        </Button>
+
+        <Link href="#"> Forgot Password </Link>
+        
+        <Router>
+          <Link component={RouterLink} to="/Registration">
+            Create an Account
+          </Link>
+        </Router>
+      </Box>
+    </div>
+  );
+}
+
+export default Login;
