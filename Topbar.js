43dbb95f961643cfbafe4359b221c4eb6aa75610
jennypham <64534681+jennyberry@users.noreply.github.com>
2021-11-30 21:43:18 +1100

    Update topbar

--- Topbar.js
+++ Topbar.js
@@ -1,13 +1,46 @@
-import { AppBar, Toolbar } from '@mui/material';
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Avatar } from '@mui/material';
+import Stack from '@mui/material/Stack';
 
- const Topbar = () => {
+
+export default function ButtonAppBar() {
   return (
-    <div>
-      <AppBar>
-        <Toolbar></Toolbar>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            SitePlan Design
+          </Typography>
+          <Stack direction="row" spacing={2}>
+          <Button color="inherit">Upload</Button>
+          <IconButton 
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}>
+          <Avatar>R</Avatar>
+          </IconButton>
+          </Stack>
+          
+        </Toolbar>
       </AppBar>
-    </div>
-  )
+    </Box>
+  );
 }
-export default Topbar;
