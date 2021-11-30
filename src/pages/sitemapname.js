f5998735f8b1e74dd8f0ce135d3a50b3bb100b82
Afnaan10 <95291805+Afnaan10@users.noreply.github.com>
2021-12-01 00:50:21 +1100

    Create sitemapname.js

---
+++ src/pages/sitemapname.js
@@ -0,0 +1,73 @@
+import React from "react";
+import "./sitemapname.css";
+import img1 from "../images/logo.png";
+import Button from "@mui/material/Button";
+import TextField from "@material-ui/core/TextField";
+import { AccountCircle, LockRounded } from "@material-ui/icons";
+import { InputAdornment } from "@mui/material";
+import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
+import { makeStyles } from "@material-ui/core";
+import { theme } from "../theme";
+
+const useStyles = makeStyles((theme) => ({
+  textfield: {
+    margin: "normal",
+    size: "small",
+    variant: "outlined",
+    color: "primary",
+    paddingBottom: "10px",
+  },
+}));
+
+function sitemapname() {
+  const classes = useStyles();
+  return (
+    <div className="sitemapname-form">
+      <Box
+        height="75px"
+        alignItems="center"
+        display="flex"
+        flexDirection="column"
+        marginTop="100px"
+        padding="10px"
+      >
+        <img src={img1} alt="" />
+      </Box>
+      <Box
+        sx={{
+          margin: "auto",
+          border: 1,
+          borderRadius: "10%",
+          width: "25rem",
+          height: "25rem",
+          justifyContent: "center",
+          marginTop: 5,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        
+   
+        <Button
+          variant="contained"
+          size="medium"
+          style={{
+            backgroundColor: theme.palette.primary.main,
+            minWidth: "225px",
+            minHeight: "30px",
+            maxWidth: "225px",
+            maxHeight: "30px",
+          }}
+        >
+          sitemapname
+        </Button>
+
+      
+      </Box>
+    </div>
+  );
+}
+
+export default Login;
