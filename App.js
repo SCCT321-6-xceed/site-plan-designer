2cf826a8f720f9bc633d36e556646ed7c3f57f27
jennypham <64534681+jennyberry@users.noreply.github.com>
2021-12-04 17:31:30 +1100

    update dashboard and library

--- App.js
+++ App.js
@@ -1,28 +1,20 @@
 import React from 'react'
-import { Avatar, Typography } from '@mui/material';
-import { Grid,Stack, Box,Toolbar,Container,Link } from '@mui/material';
-import { AppBar, Button, Card, CardActions,CardContent, CardMedia,CssBaseline } from '@mui/material';
-import ModeEditRoundedIcon from '@mui/icons-material/ModeEditRounded';
-import Topbar from './component/Topbar';
-import Main from './component/Main';
-import Leftbar from './component/Leftbar';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Dashboard from './Dashboard';
+import Library from './Library';
 const App = () => {
- 
+
   return (
-    <div>
-       <Topbar/>
-      <Grid container spacing={1}>
-        <Grid item  xs={2} sm={4} md={2}>
-          <Leftbar />
-        </Grid>
-        <Grid item  xs={10} sm={8} md={10}>
-        <Main/>
-        </Grid>
-    
-    </Grid> 
-    </div>
-   
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Dashboard />} />
+          <Route exact path="/Library" element={<Library />} />
+        </Routes>
+      </Router>
+
+    </>
+
   )
 }
 export default App;
