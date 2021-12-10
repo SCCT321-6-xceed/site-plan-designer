811dd18e82ff335b417bf64969f3091f3b53aab1
prabin2700 <36401540+prabin2700@users.noreply.github.com>
2021-12-11 02:01:10 +1100

    testing

--- src/App.js
+++ src/App.js
@@ -4,25 +4,34 @@ import Dashboard from "./pages/Dashboard.js";
 import Navbar from "./components/Navbar.js";
 import { theme } from "./theme";
 import { ThemeProvider } from "@material-ui/styles";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Router>
-    <ThemeProvider theme={theme}>
-        <div className="form-content">
-          <Routes>
-            <Route exact path="/" element={<Login />}/>
-            <Route exact path="/Dashboard" element ={<><Navbar /><Dashboard /></>}/>
-            <Route exact path="/registration" element={<Registration />}/>
-          </Routes>
-        </div>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <div className="form-content">
+            <Routes>
+              <Route exact path="/" element={<Login />} />
+              <Route
+                exact
+                path="/Dashboard"
+                element={
+                  <>
+                    <Navbar />
+                    <Dashboard />
+                  </>
+                }
+              />
+              <Route exact path="/registration" element={<Registration />} />
+            </Routes>
+          </div>
         </ThemeProvider>
-    </Router>
-     
+      </Router>
     </>
   );
 }
+//Testing GIT
 
 export default App;
