ec495c7cc76f36f164cf9d50f710d791c586d65e
prabin2700 <36401540+prabin2700@users.noreply.github.com>
2021-12-14 20:46:01 +1100

    Update Export.js

--- src/pages/Export.js
+++ src/pages/Export.js
@@ -3,20 +3,20 @@ import ExportNav from "../exportComponent/ExportNav";
 import { Grid } from "@mui/material";
 import { Typography } from "@material-ui/core";
 import MainPlan from "../planComponent/MainPlan";
-import ExportRightBar from "../exportComponent/ExportRightBar";
+import ExportRightBar from "../exportComponent/ExportRightbar.js";
 function Export() {
   return (
     <div>
       <ExportNav />
       <Grid container spacing={1}>
         <Grid item xs={9} sm={8} md={8}>
-        <Typography
-       variant='h5'
-       align='center'
-       style={{paddingTop:'20px', color:'#044474'}}
-       >
-         Project name
-       </Typography>
+          <Typography
+            variant="h5"
+            align="center"
+            style={{ paddingTop: "20px", color: "#044474" }}
+          >
+            Project name
+          </Typography>
           <MainPlan />
         </Grid>
         <Grid item xs={3} sm={4} md={4}>
